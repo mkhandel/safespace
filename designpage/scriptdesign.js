64a6ccfd383fa4f501bb15eee91ca5f5c1359290
mkhandel <mkhandel@wellesley.edu>
2024-02-29 16:35:10 -0700

    added carousel
    on design page, added two buttons (forward and backward) to move through background images

--- designpage/scriptdesign.js
+++ designpage/scriptdesign.js
@@ -13,7 +13,7 @@ document.querySelectorAll("li").forEach(el => {
 document.querySelectorAll("li").forEach(el => {
   el.addEventListener("mouseout", () => {
     //change color of list element back when not hovering over it
-    document.body.style.background = "pink";
+    document.body.style.backgroundImage = "url('./imagesdesign/purplewaterfall.png')";
     el.style.color = "black";
     //make my image reappear once stopped hovering
     document.querySelector("#waterfall").style.visibility = "visible";
@@ -37,9 +37,52 @@ document.querySelector("#noteli").addEventListener("mouseout", () => {
 );
 
 //change background
-let background = document.querySelector("#backgroundImage");
-console.log(background);
-console.log(background.src);
-let imageSource = localStorage.getItem("backgroundURL");
-background.src = imageSource;
-localStorage.setItem("backgroundURL", imageSource);
+// let background = document.body.style.backgroundImage;
+// console.log(background);
+// console.log(background.src);
+// let imageSource = localStorage.getItem("backgroundURL");
+// background.src = imageSource;
+// localStorage.setItem("backgroundURL", imageSource);
+
+
+
+//carousel
+let images = ["../assets/beach.png", "../assets/bedroom.png", "../assets/fallcolors.png", 
+"../assets/icywaterfall.png", "../assets/moonlight.png", "../assets/park.png", "../assets/purplewaterfall.png", 
+"../assets/smalltown.png", "../assets/stable.png", "../assets/sunrise.png", "../assets/sunsetbeach.png", "../assets/texturedroom.png"];
+let activeIndex = 0;
+
+// if user has seen website, and they have image stored in local storage
+if ( localStorage.getItem("backgroundURL")) { //localStorage.backgroundImage != null) {
+  let activeSource = localStorage.getItem("backgroundURL");
+  document.body.style.backgroundImage = `url(${activeSource})`;
+  activeIndex = images.indexOf(activeSource);
+  console.log("in if block");
+} else { //if user has never been to website, we set image for them
+  localStorage.setItem("backgroundURL", images[activeIndex]);
+  let activeSource = localStorage.getItem("backgroundURL");
+  document.body.style.backgroundImage = `url(${activeSource})`;
+  console.log("in else block");
+}
+
+//functionality for carousel buttons
+
+document.querySelector("#backButton").addEventListener("click", () => {
+  console.log(backButton);
+  if (activeIndex == 0) {
+    activeIndex = images.length-1;
+  } else {
+    activeIndex--;
+  }
+  document.body.style.backgroundImage = `url(${images[activeIndex]})`;
+})
+
+document.querySelector("#nextButton").addEventListener("click", () => {
+  console.log(nextButton);
+  if (activeIndex == images.length - 1) {
+    activeIndex = 0;
+  } else {
+    activeIndex++;
+  }
+  document.body.style.backgroundImage = `url(${images[activeIndex]})`;
+})
