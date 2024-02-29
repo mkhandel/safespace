ba6c45adc8b2701fb5e5e909801b309914358e33
mkhandel <mkhandel@wellesley.edu>
2024-02-29 16:50:32 -0700

    fixed buggy carousel
    when mouseout of any button, image should revert back to normal

--- designpage/scriptdesign.js
+++ designpage/scriptdesign.js
@@ -1,51 +1,3 @@
-
-
-document.querySelectorAll("li").forEach(el => {
-  el.addEventListener("mouseover", () => {
-    //change color of list element when hovering over it
-    document.body.style.background = "black";
-    el.style.color = "white";
-    //make my image disappear when hovering over any li element...
-    document.querySelector("#waterfall").style.visibility = "hidden";
-  })
-});
-
-document.querySelectorAll("li").forEach(el => {
-  el.addEventListener("mouseout", () => {
-    //change color of list element back when not hovering over it
-    document.body.style.backgroundImage = "url('./imagesdesign/purplewaterfall.png')";
-    el.style.color = "black";
-    //make my image reappear once stopped hovering
-    document.querySelector("#waterfall").style.visibility = "visible";
-  })
-});
-
-
-document.querySelector("#backgroundli").addEventListener("mouseover", () => {
-    document.querySelector("#waterfall").style.visibility = "visible";
-  }
-);
-
-document.querySelector("#noteli").addEventListener("mouseover", () => {
-    document.querySelector("#notesbubble").style.visibility = "visible";
-  }
-);
-
-document.querySelector("#noteli").addEventListener("mouseout", () => {
-    document.querySelector("#notesbubble").style.visibility = "hidden";
-  }
-);
-
-//change background
-// let background = document.body.style.backgroundImage;
-// console.log(background);
-// console.log(background.src);
-// let imageSource = localStorage.getItem("backgroundURL");
-// background.src = imageSource;
-// localStorage.setItem("backgroundURL", imageSource);
-
-
-
 //carousel
 let images = ["../assets/beach.png", "../assets/bedroom.png", "../assets/fallcolors.png", 
 "../assets/icywaterfall.png", "../assets/moonlight.png", "../assets/park.png", "../assets/purplewaterfall.png", 
@@ -86,3 +38,66 @@ document.querySelector("#nextButton").addEventListener("click", () => {
   }
   document.body.style.backgroundImage = `url(${images[activeIndex]})`;
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.querySelectorAll("li").forEach(el => {
+  el.addEventListener("mouseover", () => {
+    //change color of list element when hovering over it
+    document.body.style.background = "black";
+    el.style.color = "white";
+    //make my image disappear when hovering over any li element...
+    document.querySelector("#waterfall").style.visibility = "hidden";
+  })
+});
+
+document.querySelectorAll("li").forEach(el => {
+  el.addEventListener("mouseout", () => {
+    //change color of list element back when not hovering over it
+    document.body.style.backgroundImage = `url(${images[activeIndex]})`;
+    el.style.color = "black";
+  })
+});
+
+
+document.querySelector("#backgroundli").addEventListener("mouseover", () => {
+    document.querySelector("#waterfall").style.visibility = "visible";
+  }
+);
+
+document.querySelector("#noteli").addEventListener("mouseover", () => {
+    document.querySelector("#notesbubble").style.visibility = "visible";
+  }
+);
+
+document.querySelector("#noteli").addEventListener("mouseout", () => {
+    document.querySelector("#notesbubble").style.visibility = "hidden";
+  }
+);
+
+//change background
+// let background = document.body.style.backgroundImage;
+// console.log(background);
+// console.log(background.src);
+// let imageSource = localStorage.getItem("backgroundURL");
+// background.src = imageSource;
+// localStorage.setItem("backgroundURL", imageSource);
+
+
+
