ec5ec44b6c7b78f6b478c3e8f3827139077fe38b
mkhandel <mkhandel@wellesley.edu>
2024-03-07 15:36:23 -0700

    menu slides away when home is clicked on landing page

--- scriptindex.js
+++ scriptindex.js
@@ -7,3 +7,8 @@ document.querySelector(".toggleactive").addEventListener("click", () => {
     document.querySelector(".right").style.animation = "moveright 1.5s ease-in";
     document.querySelector(".right").style.animationFillMode = "forwards";
 })
+
+document.querySelector(".home").addEventListener("click", () => {
+    document.querySelector(".right").style.animation = "moveright 1.5s ease-in";
+    document.querySelector(".right").style.animationFillMode = "forwards";
+})
