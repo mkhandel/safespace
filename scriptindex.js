d85d503a936fe5bfe058858528ca3c8768f16053
mkhandel <mkhandel@wellesley.edu>
2024-03-07 11:27:42 -0700

    completed menu

---
+++ scriptindex.js
@@ -0,0 +1,7 @@
+document.querySelector(".toggle").addEventListener("click", () => {
+    document.querySelector(".right").style.display = "block";
+})
+
+document.querySelector(".toggleactive").addEventListener("click", () => {
+    document.querySelector(".right").style.display = "none";
+})
