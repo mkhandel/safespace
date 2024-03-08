ef68e78c9e99e989cd3d434e22cfa15f10eefb57
mkhandel <mkhandel@wellesley.edu>
2024-03-08 12:05:47 -0700

    menu slides back before new page shows

--- script.js
+++ script.js
@@ -11,9 +11,22 @@ document.querySelector(".toggleactive").addEventListener("click", () => {
 })
 
 //if clicked on menu item
-document.querySelector(".about").addEventListener("click", () => {
+document.querySelector(".menu").addEventListener("click", (e) => {
     console.log("in the file name click event listener");
     document.querySelector(".right").style.animation = "moveright 1.5s ease-in";
     document.querySelector(".right").style.animationFillMode = "forwards";
-    location.href="about.html";
+
+    //wait for menu to slide backout to switch html pages
+    setTimeout(function()
+    {
+        var path = window.location.pathname;
+        var page = path.split("/").pop();
+        console.log( page );
+        console.log("here is inverted function output:");
+        console.log(e.target.className);
+
+        if (page != e.target.className) {
+            location.href=e.target.className+".html";
+        }
+    }, 1500);
 })
